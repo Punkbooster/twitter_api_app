232df5120bee55bd8e178c631d32825ac6a55fc5
Punkbooster <2morrow2late@rambler.ru>
2016-03-12 20:55:48 +0100

    Show username from twitter on index & log out

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
 
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
 
+  match 'signout', to: 'sessions#destroy', via: [:get, :post], as: 'signout'
+
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+
 end
